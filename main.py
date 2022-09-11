ebaf1ce23bfb2db3394b9a6f60bfdec438f06767
Al Rahim Hossain <rahim620@students.cs.ubc.ca>
2022-09-10 19:47:12 -0800

    Created email_alert() method. Deleted previous notifier.py to have everything in main.py. TODO: create a timed loop to keep checking the website

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+from email.message import EmailMessage
 
 
-# URL of the course page
+# sends email notification
+def email_alert(subject, body, to):
+    # BURNER ACCOUNT AND PASSWORD
+    user = 'burnerr1729@gmail.com'
+    password = 'ecgchiomnyxlrgfr'
+
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+    msg['from'] = user
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+    server.quit()
+
+
+# parses the course page and checks if seat is available
+def get_availability(url):
+    # act like a browser
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/39.0.2171.95 Safari/537.36'}
+
+    # download the page
+    source = requests.get(url, headers=headers).text
+    # parse the downloaded page
+    soup = BeautifulSoup(source, 'lxml')
+
+    # find the cell containing the number of available spots and store it in seats
+    table = soup.find_all('table')[3]
+    row = table.find_all_next('tr')[0]
+    seats = row.find_all_next('td')[1].strong.text
+    seats = int(seats)
+
 url = 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=ASTR&course=200&section=101'
-
-# act like a browser
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/39.0.2171.95 Safari/537.36'}
-
-# download the page
-source = requests.get(url, headers=headers).text
-# parse the downloaded page
-soup = BeautifulSoup(source, 'lxml')
-
-# find the cell containing the number of available spots and store it in seats
-table = soup.find_all('table')[3]
-row = table.find_all_next('tr')[0]
-seats = row.find_all_next('td')[1].strong.text
-seats = int(seats)
-
-
--- notifier.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-from email.message import EmailMessage
-
-
-def email_alert(subject, body, to):
-    msg = EmailMessage()
-    msg.set_content(body)
